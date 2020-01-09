cfc2b6434ce699c560187e46b2b305558be6bdfe
Sunil Bishnoi <42268974+sunil7796@users.noreply.github.com>
2020-01-09 18:43:23 +0530

    Kafkacontroller changes

--- kafka-java/src/main/java/com/okta/javakafka/controller/KafkaController.java
+++ kafka-java/src/main/java/com/okta/javakafka/controller/KafkaController.java
@@ -1,7 +1,6 @@
 package com.okta.javakafka.controller;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.okta.javakafka.consumer.TestConsumer;
 import com.okta.javakafka.entity.Employee;
 
@@ -17,32 +19,53 @@ public class KafkaController {
 
 	private KafkaTemplate<String, String> template;
 	private TestConsumer testConsumer;
-	
+
 	/**
 	 * 
-	 * @param template to sending a message to topic
+	 * @param template     to sending a message to topic
 	 * @param testConsumer consumes every changes
 	 */
 	public KafkaController(KafkaTemplate<String, String> template, TestConsumer testConsumer) {
 		this.template = template;
 		this.testConsumer = testConsumer;
 	}
-	
+
 	/**
 	 * 
 	 * @param message, producer posts message
 	 */
 	@PostMapping("/kafka/produce")
+	// @RequestMapping(consumes="application/json")
 	public void produce(@RequestBody List<Employee> employees) {
-		template.send("test", employees.toString());
-	}
+		
+		ObjectMapper mapper = new ObjectMapper();
 	
+		for (Employee employee : employees) {
+		
+			String employeeString = null;
+			
+			try {
+				employeeString = mapper.writeValueAsString(employee);
+			} catch (JsonProcessingException e) {
+
+				e.printStackTrace();
+			}
+
+			System.out.println(employeeString);
+			template.send("test", employeeString);
+		}
+		
+	}
+
 	/**
 	 * 
-	 * @return consumed messages 
+	 * @return consumed messages
+	 * @throws JsonProcessingException 
+	 * @throws JsonMappingException 
 	 */
 	@GetMapping("/kafka/messages")
-	    public Map<String, List<Employee>> getMessages() {
-	        return testConsumer.getMessages();
-	    }
+	// @RequestMapping(produces="application/json")
+	public List<Employee> getMessages() throws JsonMappingException, JsonProcessingException {
+		return testConsumer.getMessages();
+	}
 }
