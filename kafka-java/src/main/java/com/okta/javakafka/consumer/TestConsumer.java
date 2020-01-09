66925977beca46f333cc7398045414786881d25f
Sunil Bishnoi <42268974+sunil7796@users.noreply.github.com>
2020-01-09 18:42:16 +0530

    kafkaconsumerupdate

--- kafka-java/src/main/java/com/okta/javakafka/consumer/TestConsumer.java
+++ kafka-java/src/main/java/com/okta/javakafka/consumer/TestConsumer.java
@@ -1,26 +1,38 @@
 package com.okta.javakafka.consumer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.okta.javakafka.entity.Employee;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Component
 public class TestConsumer {
-    private final Map<String, List<Employee>> messages = new HashMap<>();
+    private final List<String> messages = new ArrayList<>();
     
     @KafkaListener(topics = "test", groupId = "kafka-sandbox")
-    public void listen(List<Employee> message) {
+    public void listen(String message) {
         synchronized (messages) {
-            messages.put("Message",message);
+            messages.add(message);
         }
     }
-    public Map<String, List<Employee>> getMessages() {
-        return messages;
+    public List<Employee> getMessages() throws JsonMappingException, JsonProcessingException {
+    	ObjectMapper objectMapper = new ObjectMapper();
+    	List<Employee> employees = new ArrayList<>();
+    	Employee employee = null;
+    	for(String message : messages ) {
+    		 System.out.println(message);
+    		 employee = objectMapper.readValue(message, Employee.class);
+    		 System.out.println(employee);
+    		 employees.add(employee);
+    	}
+    	
+       
+    	return employees;
     }
 }
